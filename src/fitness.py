9de85456f8a3a183a52f1de7d70badf23bab0b7f
Ilya Korotkov <i59korotkov@gmail.com>
2023-05-08 21:48:45 +0300

    Updated fitness.py

--- src/fitness.py
+++ src/fitness.py
@@ -3,31 +3,19 @@ from typing import Tuple
 from src.game import SnakeGame
 
 
-def fitness_steps_length_alive(game: SnakeGame) -> Tuple[float, float]:
-    # Add survived steps to fitness function
-    fitness1 = game.snake1_steps_lived / 10
-    fitness2 = game.snake2_steps_lived / 10
+def fitness_time_length_alive(game: SnakeGame) -> Tuple[float, float]:
+    # Add survived time to fitness function
+    fitness1 = game.snake1.lifetime
+    fitness2 = game.snake2.lifetime
 
     # Add snakes length to fitness function
-    fitness1 += (game.snake1.qsize() - 3) * 5
-    fitness2 += (game.snake2.qsize() - 3) * 5
+    fitness1 += (game.snake1.body.qsize() - 3) * 10
+    fitness2 += (game.snake2.body.qsize() - 3) * 10
     
-    # Add 10 points to fitness function of survivors
-    if game.snake1_is_alive:
-        fitness1 += 10
-    if game.snake2_is_alive:
-        fitness2 += 10
-    
-    return fitness1, fitness2
-
-
-def fitness_steps_length(game: SnakeGame) -> Tuple[float, float]:
-    # Add survived steps to fitness function
-    fitness1 = game.snake1_steps_lived / 10
-    fitness2 = game.snake2_steps_lived / 10
-
-    # Add snakes length to fitness function
-    fitness1 += (game.snake1.qsize() - 3) * 5
-    fitness2 += (game.snake2.qsize() - 3) * 5
+    # Add 50 points to fitness function of survivors
+    if game.snake1.alive:
+        fitness1 += 50
+    if game.snake2.alive:
+        fitness2 += 50
     
     return fitness1, fitness2
