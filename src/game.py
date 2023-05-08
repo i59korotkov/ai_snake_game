c523cfae0c010201b1c25a51979c428d856f6afe
Ilya Korotkov <i59korotkov@gmail.com>
2023-05-08 21:46:20 +0300

    Refactored game.py

--- src/game.py
+++ src/game.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Optional
 from enum import Enum
 from queue import Queue
 import random
@@ -17,6 +17,17 @@ class Direction(Enum):
     RIGHT = 3
 
 
+def reverse_direction(dir: Direction) -> Direction:
+    if dir == Direction.UP:
+        return Direction.DOWN
+    elif dir == Direction.DOWN:
+        return Direction.UP
+    elif dir == Direction.LEFT:
+        return Direction.RIGHT
+    elif dir == Direction.RIGHT:
+        return Direction.LEFT
+
+
 class CellState(Enum):
     EMPTY = 0
     SNAKE1 = 1
@@ -33,123 +44,120 @@ class Colors(Enum):
     DARK_RED = (128, 0, 0)
 
 
-def reverse_direction(dir: Direction) -> Direction:
-    if dir == Direction.UP:
-        return Direction.DOWN
-    elif dir == Direction.DOWN:
-        return Direction.UP
-    elif dir == Direction.LEFT:
-        return Direction.RIGHT
-    elif dir == Direction.RIGHT:
-        return Direction.LEFT
+class SnakeInfo:
+    def __init__(
+        self,
+        body: Queue,
+        num: int,
+        alive: bool = True,
+        lifetime: int = 0,
+    ) -> None:
+        self.body = body
+        self.num = num
+        self.alive = alive
+        self.lifetime = lifetime
 
 
 class SnakeGame:
-    def __init__(self, grid_shape: Tuple[int, int] = (10, 20)) -> None:
+    def __init__(self, grid_shape: Tuple[int, int] = (10, 10)) -> None:
         if len(grid_shape) != 2 or grid_shape[0] < 5 or grid_shape[1] < 5:
             raise ValueError('Invalid grid shape')
 
         self.grid = np.zeros(grid_shape, dtype=int)
-        self.step = 0
-        self.__init_snakes()
-        self.__generate_food()
+        self.time = 0
+        self._init_snakes()
+        self._generate_food()
     
-    def __init_snakes(self):
-        self.snake1 = Queue()
-        self.snake2 = Queue()
-        self.snake1_is_alive = True
-        self.snake2_is_alive = True
-        self.snake1_steps_lived = 0
-        self.snake2_steps_lived = 0
+    def _init_snakes(self):
+        self.snake1 = SnakeInfo(Queue(), CellState.SNAKE1.value)
+        self.snake2 = SnakeInfo(Queue(), CellState.SNAKE2.value)
 
         # Init snakes on random Y positions
         y_pos1 = random.randint(1, self.grid.shape[0] - 2)
         y_pos2 = random.randint(1, self.grid.shape[0] - 2)
         for x in range(3):
-            self.snake1.put((y_pos1, 1 + x))
-            self.snake2.put((y_pos2, self.grid.shape[1] - 2 - x))
+            self.snake1.body.put((y_pos1, 1 + x))
+            self.snake2.body.put((y_pos2, self.grid.shape[1] - 2 - x))
         
-        for cell in self.snake1.queue:
+        for cell in self.snake1.body.queue:
             self.grid[cell] = CellState.SNAKE1.value
-        for cell in self.snake2.queue:
+        for cell in self.snake2.body.queue:
             self.grid[cell] = CellState.SNAKE2.value
 
-    def __move_snake(self, snake: Queue, dir: Direction, snake_num: int) -> bool:
-        if snake_num == 1 and not self.snake1_is_alive or snake_num == 2 and not self.snake2_is_alive:
-            return False
+    def _move_snake(self, snake: SnakeInfo, dir: Direction):
+        if not snake.alive:
+            return
+
+        snake.lifetime += 1
 
         # Get new head position
         if dir == Direction.UP:
-            head = snake.queue[-1]
+            head = snake.body.queue[-1]
             head = (head[0] - 1, head[1])
         elif dir == Direction.DOWN:
-            head = snake.queue[-1]
+            head = snake.body.queue[-1]
             head = (head[0] + 1, head[1])
         elif dir == Direction.LEFT:
-            head = snake.queue[-1]
+            head = snake.body.queue[-1]
             head = (head[0], head[1] - 1)
         elif dir == Direction.RIGHT:
-            head = snake.queue[-1]
+            head = snake.body.queue[-1]
             head = (head[0], head[1] + 1)
 
-        if head == snake.queue[-2]:
+        if head == snake.body.queue[-2]:
             # Reverse direction, if the movement was inside the snake body
-            return self.__move_snake(snake, reverse_direction(dir), snake_num)
+            self._move_snake(snake, reverse_direction(dir))
+            return
         
         if 0 <= head[0] < self.grid.shape[0] and 0 <= head[1] < self.grid.shape[1]:
             if self.grid[head] == CellState.EMPTY.value:
                 # If new head cell is empty
-                tail = snake.get()
-                snake.put(head)
+                tail = snake.body.get()
+                snake.body.put(head)
                 self.grid[tail] = 0
-                self.grid[head] = snake_num
-                return True
+                self.grid[head] = snake.num
             elif self.grid[head] == CellState.FOOD.value:
                 # If new head cell has food in it
-                snake.put(head)
-                self.grid[head] = snake_num
-                return True
+                snake.body.put(head)
+                self.grid[head] = snake.num
             else:
                 # If new head cell is body of a snake
-                return False
+                snake.alive = False
         else:
             # If new head cell is outside of the grid
-            return False
+            snake.alive = False
 
-    def __generate_food(self):
+    def _generate_food(self):
         empty_cells = np.where(self.grid == CellState.EMPTY.value)
         random_index = np.random.randint(len(empty_cells[0]), size=1)[0]
         self.grid[empty_cells[0][random_index], empty_cells[1][random_index]] = CellState.FOOD.value
 
-    def update(self, snake1_move_dir: Direction, snake2_move_dir: Direction):
+    def update(self, snake1_move_dir: Optional[Direction], snake2_move_dir: Optional[Direction]):
         # Randomly select which snake moves first
         if random.random() > 0.5:
-            self.snake1_is_alive = self.__move_snake(self.snake1, snake1_move_dir, CellState.SNAKE1.value)
-            self.snake2_is_alive = self.__move_snake(self.snake2, snake2_move_dir, CellState.SNAKE2.value)
+            self._move_snake(self.snake1, snake1_move_dir)
+            self._move_snake(self.snake2, snake2_move_dir)
         else:
-            self.snake2_is_alive = self.__move_snake(self.snake2, snake2_move_dir, CellState.SNAKE2.value)
-            self.snake1_is_alive = self.__move_snake(self.snake1, snake1_move_dir, CellState.SNAKE1.value)
-        
-        if self.snake1_is_alive:
-            self.snake1_steps_lived += 1
-        if self.snake2_is_alive:
-            self.snake2_steps_lived += 1
+            self._move_snake(self.snake2, snake2_move_dir)
+            self._move_snake(self.snake1, snake1_move_dir)
 
         if not (self.grid == CellState.FOOD.value).any():
             # Generate food if it was eaten
-            self.__generate_food()
+            self._generate_food()
 
-        self.step += 1
-        if not self.snake1_is_alive and not self.snake2_is_alive:
-            # End game if any snake died
+        self.time += 1
+        if not self.snake1.alive and not self.snake2.alive:
+            # End game if both snakes die
             raise GameEndException()
     
     def get_image(self) -> np.array:
         image = np.zeros((*self.grid.shape, 3), dtype=int)
         image[np.where(self.grid == CellState.EMPTY.value)] = Colors.BLACK.value
-        image[np.where(self.grid == CellState.SNAKE1.value)] = Colors.BLUE.value
-        image[self.snake1.queue[-1]] = Colors.DARK_BLUE.value
-        image[np.where(self.grid == CellState.SNAKE2.value)] = Colors.RED.value
-        image[self.snake2.queue[-1]] = Colors.DARK_RED.value
+        if self.snake1.body.qsize() > 0:
+            image[np.where(self.grid == CellState.SNAKE1.value)] = Colors.BLUE.value
+            image[self.snake1.body.queue[-1]] = Colors.DARK_BLUE.value
+        if self.snake2.body.qsize() > 0:
+            image[np.where(self.grid == CellState.SNAKE2.value)] = Colors.RED.value
+            image[self.snake2.body.queue[-1]] = Colors.DARK_RED.value
         image[np.where(self.grid == CellState.FOOD.value)] = Colors.WHITE.value
         return image
