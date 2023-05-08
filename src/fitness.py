4e88fad5b5e2ffb09a40d0ee181bd1348c3a23b5
Ilya Korotkov <i59korotkov@gmail.com>
2023-05-08 19:31:09 +0300

    Bugfixes and update in fitness functions

--- src/fitness.py
+++ src/fitness.py
@@ -5,12 +5,12 @@ from src.game import SnakeGame
 
 def fitness_steps_length_alive(game: SnakeGame) -> Tuple[float, float]:
     # Add survived steps to fitness function
-    fitness1 = game.step / 10
-    fitness2 = game.step / 10
+    fitness1 = game.snake1_steps_lived / 10
+    fitness2 = game.snake2_steps_lived / 10
 
     # Add snakes length to fitness function
-    fitness1 += game.snake1.qsize() 
-    fitness2 += game.snake2.qsize()
+    fitness1 += (game.snake1.qsize() - 3) * 5
+    fitness2 += (game.snake2.qsize() - 3) * 5
     
     # Add 10 points to fitness function of survivors
     if game.snake1_is_alive:
@@ -23,11 +23,11 @@ def fitness_steps_length_alive(game: SnakeGame) -> Tuple[float, float]:
 
 def fitness_steps_length(game: SnakeGame) -> Tuple[float, float]:
     # Add survived steps to fitness function
-    fitness1 = game.step / 10
-    fitness2 = game.step / 10
+    fitness1 = game.snake1_steps_lived / 10
+    fitness2 = game.snake2_steps_lived / 10
 
     # Add snakes length to fitness function
-    fitness1 += game.snake1.qsize() 
-    fitness2 += game.snake2.qsize()
+    fitness1 += (game.snake1.qsize() - 3) * 5
+    fitness2 += (game.snake2.qsize() - 3) * 5
     
     return fitness1, fitness2
